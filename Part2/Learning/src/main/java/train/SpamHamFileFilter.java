fe1c064b0cb9dcd9ee1d74f9a8a048c13ec43089
Michael Aquilina <michaelaquilina@gmail.com>
2013-12-03 14:06:22 +0000

    Minor fix to java warning in SpamHamFileFilter

--- Part2/Learning/src/main/java/train/SpamHamFileFilter.java
+++ Part2/Learning/src/main/java/train/SpamHamFileFilter.java
@@ -7,6 +7,7 @@ import java.io.FilenameFilter;
  * Simple filename filter that only accepts Files that are either ham or spam
  */
 public class SpamHamFileFilter implements FilenameFilter {
+    @Override
     public boolean accept(File dir, String name) {
         return name.matches("(spam|ham)\\d+.txt");
     }
