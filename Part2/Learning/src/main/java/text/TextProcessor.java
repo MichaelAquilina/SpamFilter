ae7e1f5a515937b6007903ff6a6b95ff0f221cb2
Michael Aquilina <michaelaquilina@gmail.com>
2013-12-03 10:20:28 +0000

    Add docstrings to TextProcessor

--- Part2/Learning/src/main/java/text/TextProcessor.java
+++ Part2/Learning/src/main/java/text/TextProcessor.java
@@ -1,6 +1,7 @@
 package text;
 
-// This class should extended
+// This class should extended with any text pre or post processing methods
+// such as spell checking, stemming, lemmatisation etc...
 public class TextProcessor {
     private final Stemmer stemmer;
     
@@ -13,6 +14,7 @@ public class TextProcessor {
         throw new UnsupportedOperationException();
     }
     
+    // Weak Stemming algorithm - prefer porterStem over this
     public String weakStem(String word) {
         // TODO: Test performance with and without weak stemmer
         // TODO: Add more suffixes
@@ -23,6 +25,13 @@ public class TextProcessor {
         return word;
     }
     
+    /***
+     * Porter Stemmer algorithm designed and developed by M.F. Porter 
+     * http://tartarus.org/martin/PorterStemmer/. Only works with English
+     * words - will give inaccurate results with other languages.
+     * @param word String word (in English) to stem.
+     * @return String stem of the input word.
+     */
     public String porterStem(String word) {
         stemmer.add(word.toCharArray(), word.length());
         stemmer.stem();
