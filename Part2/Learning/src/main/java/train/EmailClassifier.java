4a4941df38d91a4d71a3ad6fb3e04261cd74f51b
Michael Aquilina <michaelaquilina@gmail.com>
2013-12-04 21:16:52 +0000

    Add EmailClassifier capable of training

--- Part2/Learning/src/main/java/train/EmailClassifier.java
+++ Part2/Learning/src/main/java/train/EmailClassifier.java
@@ -2,32 +2,37 @@ package train;
 
 import classification.Classifier;
 import classification.Email;
+import classification.EmailClass;
+import classification.LabelledVector;
 import invertedindex.HashedIndex;
 import invertedindex.InvertedIndex;
 import text.Parser;
 import text.TextProcessor;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 
 public class EmailClassifier {
 
     private Classifier classifier;
     private InvertedIndex invertedIndex;
 
-    public EmailClassifier(Classifier classifier, InvertedIndex invertedIndex) {
+    public EmailClassifier(Classifier classifier) {
         this.classifier = classifier;
-        this.invertedIndex = invertedIndex;
+        this.invertedIndex = new HashedIndex();
     }
 
     public void train(String trainingPath) throws IOException {
 
+        invertedIndex.clear();
+
+        // TODO: Make these values alterable
         float upperPercentile = 0.9f;
         float lowerPercentile = 0.07f;
 
         // Part 1: Parsing and pre-processing of text
         Parser parser = new Parser();
-        InvertedIndex stopwordIndex = new HashedIndex();
+        InvertedIndex stopWordIndex = loadStopWordsIndex();
 
         File trainingDirectory = new File(trainingPath);
         File[] trainingFiles = trainingDirectory.listFiles(new SpamHamFileFilter());
@@ -37,8 +42,8 @@ public class EmailClassifier {
             Email email = parser.parseFile(trainingFile);
             for(String term : email.getWords()) {
 
-                if(!stopwordIndex.containsTerm(term)) {
-                    if(!TextProcessor.isNumber(term)) {
+                if(!stopWordIndex.containsTerm(term)) {
+                    if(TextProcessor.isNumber(term)) {
                         invertedIndex.add("9999", trainingFile.getName());
                     }
                     else
@@ -51,11 +56,76 @@ public class EmailClassifier {
         }
 
         // Part 2: Feature Selection
-        int minDocFrequency = (int) (lowerPercentile * invertedIndex.documentCount());
-        int maxDocFrequency = (int) (upperPercentile * invertedIndex.documentCount());
+        int minDocFrequency = (int) (lowerPercentile * invertedIndex.getDocumentCount());
+        int maxDocFrequency = (int) (upperPercentile * invertedIndex.getDocumentCount());
         invertedIndex.trimIndex(minDocFrequency, maxDocFrequency);
 
         // Part 3: Feature Weighting
-        
+        ArrayList<LabelledVector> vectors = extractVectors();
+
+        // Part 4: Classifier Training
+        classifier.train(vectors);
+    }
+
+    public EmailClass classify(String emailPath) {
+        return EmailClass.Ham;
+    }
+
+    public int getTermCount() {
+        return invertedIndex.getTermCount();
+    }
+
+    public int getDocumentCount() {
+        return invertedIndex.getDocumentCount();
+    }
+
+    // calculate tfidf value with given input parameters
+    // Using augmented frequency as found on wikipedia http://en.wikipedia.org/wiki/Tf%E2%80%93idf
+    private double tfidf(String term, String document)
+    {
+        double tf = 0.5 + 0.5 * ((double) invertedIndex.getTermFrequency(term, document) / (double) invertedIndex.getMaxTermFrequency());
+        double idf = Math.log((double) invertedIndex.getDocumentCount() / (double) (1 + invertedIndex.getDocumentFrequency(term)));
+
+        return tf * idf;
+    }
+
+    // Extracts labelled vectors from the current inverted index
+    private ArrayList<LabelledVector> extractVectors() {
+        ArrayList<LabelledVector> vectors = new ArrayList<>();
+
+        for(String emailDocument : invertedIndex.getDocuments()) {
+            LabelledVector emailVector = new LabelledVector();
+            emailVector.setEmailClass(emailDocument.contains("ham")? EmailClass.Ham : EmailClass.Spam);
+
+            double[] vectorData = new double[invertedIndex.getTermCount()];
+
+            // I think we can assume that the index will remain the same
+            int index = 0;
+            for(String term : invertedIndex.getTerms()) {
+                vectorData[index++] = invertedIndex.getTermFrequency(term, emailDocument);
+            }
+
+            emailVector.setVector(vectorData);
+            vectors.add(emailVector);
+        }
+
+        return vectors;
+    }
+
+    //TODO: Make this class less dependent on external files
+    private InvertedIndex loadStopWordsIndex() throws IOException {
+        InvertedIndex stopwordsIndex = new HashedIndex();
+
+        InputStream stream = Train.class.getResourceAsStream("stopwords.txt");
+
+        BufferedReader stopwords = new BufferedReader(new InputStreamReader(stream));
+
+        String line;
+        while((line = stopwords.readLine()) != null)
+            stopwordsIndex.add(line, "stopwords.txt");
+
+        stopwords.close();
+
+        return stopwordsIndex;
     }
 }
--- Part2/Learning/src/main/java/train/Train.java
+++ Part2/Learning/src/main/java/train/Train.java
@@ -1,16 +1,11 @@
 package train;
 
-import classification.Classifier;
-import classification.Email;
 import classification.EmailClass;
-import classification.LabelledVector;
 import classification.NaiveBayes;
-import invertedindex.HashedIndex;
-import invertedindex.InvertedIndex;
 import text.Parser;
 
-import java.io.*;
-import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,114 +14,56 @@ public class Train {
         System.out.println("Usage: ");
         System.out.println("\tjava -jar spamfilter-learning.jar <traindata> <outfile>");
     }
-    
-    public static InvertedIndex loadStopWordsIndex() {
-        InvertedIndex stopwordsIndex = new HashedIndex(); 
-        
-        InputStream stream = Train.class.getResourceAsStream("stopwords.txt");
-        
-        BufferedReader stopwords = new BufferedReader(new InputStreamReader(stream));
-        
-        try {
-            String line;
-            while((line = stopwords.readLine()) != null)
-                stopwordsIndex.add(line, "stopwords.txt");
-            
-            stopwords.close();
-        } catch(IOException e) {
-            System.err.println("Error reading from stopwords index");
-            e.printStackTrace();
-            return null;
-        }
-        
-        return stopwordsIndex;
-    }
 
     public static void main(String[] args) {
-       if (args.length != 2) {
-           usage();
-           return;
-       }
-       
-       // Load Stopwords Index
-       InvertedIndex stopwordsIndex = loadStopWordsIndex();
+        if (args.length != 2) {
+            usage();
+            return;
+        }
 
-       Parser parser = new Parser();
-       InvertedIndex invertedIndex = new HashedIndex();   // We can swap in the future if needs be
+        String trainingPath = args[0];
+        String stateFilePath = args[1];
        
-       String trainDataPath = args[0];
-       String outFilePath = args[1];
+        EmailClassifier emailClassifier = new EmailClassifier(new NaiveBayes());
 
-       File directory = new File(trainDataPath);
-       File[] examples = directory.listFiles(new SpamHamFileFilter());
+        try {
+            emailClassifier.train(trainingPath);
 
-       for(File example : examples) {
-           try {
-               Email email = parser.parseFile(example);
-               
-               for(String word : email.getWords()) {
-                   if(!stopwordsIndex.containsTerm(word)) {
-                       String term = VectorFactory.transform(word);
+            System.out.format("Classifier trained with %d emails\n", emailClassifier.getDocumentCount());
+            System.out.format("Classifier Dimensions = %d\n", emailClassifier.getTermCount());
+        } catch(IOException e) {
+            System.err.println("An Error occurred while trying to perform training");
+            e.printStackTrace();
+            return;
+        }
 
-                       if(term != null)
-                           invertedIndex.add(term, example.getName());
-                   }
-               }
-               
-               System.out.format("Successfully loaded %d terms for %s with class %s\n", email.getWords().size(), example.getName(), email.getEmailClass().toString());
-               
-           } catch (FileNotFoundException e) {
-               System.err.println(String.format("Could not load \"%s\"", example.getName()));
-               e.printStackTrace();
-           } catch (IOException e) {
-               System.err.println(String.format("IO Error during loading \"%s\"", example.getName()));
-               e.printStackTrace();
-           } 
-       }
-        
-       System.out.format("Final InvertedIndex term size = %d\n", invertedIndex.termCount());
-       
-       System.out.println("Trimming Index...");
-       
-       int min = (int) (invertedIndex.documentCount() * 0.07);
-       int max = (int) (invertedIndex.documentCount() * 0.9);
-       
-       invertedIndex.trimIndex(min, max);
-       
-       System.out.format("Trimmed Inverted Index term size = %d\n", invertedIndex.termCount());
+        // TODO@Uwe: Cross-Validation possibly a better approach?
+        // TODO@Uwe: Do a better implementation of this.
+        Map<EmailClass, Map<EmailClass, Integer>> confusion = new HashMap<EmailClass, Map<EmailClass, Integer>>();
+        confusion.put(EmailClass.Spam, new HashMap<EmailClass, Integer>());
+        confusion.get(EmailClass.Spam).put(EmailClass.Spam, 0);
+        confusion.get(EmailClass.Spam).put(EmailClass.Ham, 0);
+        confusion.put(EmailClass.Ham, new HashMap<EmailClass, Integer>());
+        confusion.get(EmailClass.Ham).put(EmailClass.Spam, 0);
+        confusion.get(EmailClass.Ham).put(EmailClass.Ham, 0);
 
-       VectorFactory vectorise = new VectorFactory(invertedIndex);
-       
-       ArrayList<LabelledVector> labelledVectors = vectorise.getLabelledVectors();
+        // @Uwe: Is this temporary?
+        Parser parser = new Parser();
 
-       // IMPORTANT
-       // Use the Classifier class as a common interface for performing training
-       Classifier someClassifier = new NaiveBayes();
-       someClassifier.train(labelledVectors);
-       
-       Map<EmailClass, Map<EmailClass, Integer>> confusion = new HashMap<EmailClass, Map<EmailClass, Integer>>();
-       confusion.put(EmailClass.Spam, new HashMap<EmailClass, Integer>());
-       confusion.get(EmailClass.Spam).put(EmailClass.Spam, 0);
-       confusion.get(EmailClass.Spam).put(EmailClass.Ham, 0);
-       confusion.put(EmailClass.Ham, new HashMap<EmailClass, Integer>());
-       confusion.get(EmailClass.Ham).put(EmailClass.Spam, 0);
-       confusion.get(EmailClass.Ham).put(EmailClass.Ham, 0);
+        File trainingDirectory = new File(trainingPath);
+        File[] trainingFiles = trainingDirectory.listFiles(new SpamHamFileFilter());
 
-       // Hack: Simply clasifiy an instance to see if the classifier works.
-       int[] vector = new int[labelledVectors.get(0).getVector().length];
-       for (LabelledVector vec : labelledVectors) {
-           for (int i = 0; i < vector.length; i++) {
-               vector[i] = (int)vec.getVector()[i];
-           }
-           EmailClass clClass = someClassifier.classify(vector);
-           EmailClass actualClass = vec.getEmailClass();
-           confusion.get(actualClass).put(clClass, confusion.get(actualClass).get(clClass) + 1);
-       }
+        for(File trainingFile : trainingFiles) {
+            EmailClass actualClass = trainingFile.getName().contains("ham")? EmailClass.Ham : EmailClass.Spam;
+            EmailClass emailClass = emailClassifier.classify(trainingFile.getAbsolutePath());
 
-       // Print confusion matrix
-       System.out.println("   |   Spam |    Ham |");
-       System.out.println("---|--------|--------|");
-       System.out.format( " S | % 6d | % 6d |\n", confusion.get(EmailClass.Spam).get(EmailClass.Spam), confusion.get(EmailClass.Spam).get(EmailClass.Ham));
-       System.out.format( " H | % 6d | % 6d |\n", confusion.get(EmailClass.Ham).get(EmailClass.Spam), confusion.get(EmailClass.Ham).get(EmailClass.Ham));
+            confusion.get(actualClass).put(emailClass, confusion.get(actualClass).get(emailClass) + 1);
+        }
+
+        // Print confusion matrix
+        System.out.println("   |   Spam |    Ham |");
+        System.out.println("---|--------|--------|");
+        System.out.format( " S | % 6d | % 6d |\n", confusion.get(EmailClass.Spam).get(EmailClass.Spam), confusion.get(EmailClass.Spam).get(EmailClass.Ham));
+        System.out.format( " H | % 6d | % 6d |\n", confusion.get(EmailClass.Ham).get(EmailClass.Spam), confusion.get(EmailClass.Ham).get(EmailClass.Ham));
     }
 }
