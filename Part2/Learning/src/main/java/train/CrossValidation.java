508f3c9ea17690f8ceaa404a538ba772d967f8eb
Uwe L. Korn <uwelk@xhochy.com>
2013-12-05 19:06:38 +0000

    Refactor Crossvalidation to do all folds by itself

--- Part2/Learning/src/main/java/train/CrossValidation.java
+++ Part2/Learning/src/main/java/train/CrossValidation.java
@@ -1,7 +1,10 @@
 package train;
 
+import classification.ConfusionMatrix;
+import classification.EmailClass;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -9,22 +12,57 @@ import java.util.List;
 
 
 public class CrossValidation {
-    private List<File> trainingFiles;
-    private List<File> testFiles;
+    private ArrayList<File> files;
+    private EmailClassifier classifier;
+    private ConfusionMatrix confusionMatrix;
+    private List<ConfusionMatrix> confusionMatrices;
 
-    public CrossValidation(String directory) {
+    public CrossValidation(String directory, EmailClassifier classifier) {
+        this.classifier = classifier;
+
         File trainingDirectory = new File(directory);
-        ArrayList<File> files = new ArrayList<File>(Arrays.asList(trainingDirectory.listFiles(new SpamHamFileFilter())));
+        files = new ArrayList<File>(Arrays.asList(trainingDirectory.listFiles(new SpamHamFileFilter())));
+    }
+
+    public void fold(int folds) throws IOException {
+        // Shuffle the list, so that we get each time other results
         Collections.shuffle(files);
-        trainingFiles = files.subList(0, (int)Math.round(files.size() * 0.9));
-        testFiles = files.subList((int)Math.round(files.size() * 0.9), files.size());
+
+        // Split into folds
+        List<List<File>> fileFolds = new ArrayList<List<File>>(folds);
+        int foldLength = files.size() / folds;
+        for (int i = 0; i < folds; i++) {
+            int end = (i == (folds - 1)) ? files.size() : ((i+1) * foldLength);
+            fileFolds.add(files.subList(i * foldLength, end));
+        }
+
+        // For each combination train and test
+        confusionMatrix = new ConfusionMatrix();
+        confusionMatrices = new ArrayList<ConfusionMatrix>();
+        for (int i = 0; i < folds; i++) {
+            // Gather training data
+            List<File> train = new ArrayList<File>();
+            for (int j = 0; j < folds; j++) {
+                if (j != i) {
+                    train.addAll(fileFolds.get(j));
+                }
+            }
+
+            classifier.train(train);
+            
+            ConfusionMatrix cm = new ConfusionMatrix();
+            for(File trainingFile : fileFolds.get(i)) {
+                EmailClass actualClass = trainingFile.getName().contains("ham")? EmailClass.Ham : EmailClass.Spam;
+                EmailClass emailClass = classifier.classify(trainingFile);
+
+                confusionMatrix.add(actualClass, emailClass);
+                cm.add(actualClass, emailClass);
+            }
+            confusionMatrices.add(cm);
+        }
     }
 
-    public List<File> getTraining() {
-        return trainingFiles;
-    }
-
-    public List<File> getTest() {
-        return testFiles;
+    public ConfusionMatrix getCombinedConfusion() {
+        return confusionMatrix;
     }
 }
--- Part2/Learning/src/main/java/train/Train.java
+++ Part2/Learning/src/main/java/train/Train.java
@@ -15,52 +15,30 @@ public class Train {
         System.out.println("\tjava -jar spamfilter-learning.jar <traindata> <outfile>");
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         if (args.length != 2) {
             usage();
             return;
         }
 
         String stateFilePath = args[1];
-        
+
         // Load the list of files and select 0.9 as training data and 0.1 as test data.
         String trainingPath = args[0];
-        CrossValidation cv = new CrossValidation(trainingPath);
-       
         EmailClassifier emailClassifier = new EmailClassifier(new NaiveBayes(), true);
+        CrossValidation cv = new CrossValidation(trainingPath, emailClassifier);
+        cv.fold(10);
+        cv.getCombinedConfusion().print();
 
-        try {
-            emailClassifier.train(cv.getTraining());
+        // try {
+        //     emailClassifier.train(cv.getTraining());
 
-            System.out.format("Classifier trained with %d emails\n", emailClassifier.getDocumentCount());
-            System.out.format("Classifier Dimensions = %d\n", emailClassifier.getTermCount());
-        } catch(IOException e) {
-            System.err.println("An Error occurred while trying to perform training");
-            e.printStackTrace();
-            return;
-        }
-
-        // TODO@Uwe: Cross-Validation possibly a better approach?
-        // TODO@Uwe: Do a better implementation of this.
-
-        // @Uwe: Is this temporary? I.e. will this step be in the final version?
-        // If so, might be better to move this code to EmailClassifier
-        // Including a method to print confusion matrices
-        try {
-            ConfusionMatrix cm = new ConfusionMatrix();
-            for(File trainingFile : cv.getTest()) {
-                EmailClass actualClass = trainingFile.getName().contains("ham")? EmailClass.Ham : EmailClass.Spam;
-                EmailClass emailClass = emailClassifier.classify(trainingFile);
-
-                cm.add(actualClass, emailClass);
-            }
-
-            cm.print();
-
-            // Print confusion matrix
-        } catch(IOException e) {
-            System.err.println("An error occurred while calculating the confusion matrix");
-            e.printStackTrace();
-        }
+        //     System.out.format("Classifier trained with %d emails\n", emailClassifier.getDocumentCount());
+        //     System.out.format("Classifier Dimensions = %d\n", emailClassifier.getTermCount());
+        // } catch(IOException e) {
+        //     System.err.println("An Error occurred while trying to perform training");
+        //     e.printStackTrace();
+        //     return;
+        // }
     }
 }
