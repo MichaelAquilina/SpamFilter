0ffc8f2c5270004c344d3d97ff337883b7c61352
Uwe L. Korn <uwelk@xhochy.com>
2013-12-05 23:59:00 +0000

    Cleanup Train

--- Part2/Learning/src/main/java/train/Train.java
+++ Part2/Learning/src/main/java/train/Train.java
@@ -1,13 +1,22 @@
 package train;
 
+import classification.Classifier;
 import classification.NaiveBayes;
 
 import java.io.IOException;
+
+import weka.Perceptron;
 
 public class Train {
     public static void usage() {
         System.out.println("Usage: ");
         System.out.println("\tjava -jar spamfilter-learning.jar <traindata> <outfile>");
+    }
+
+    public static void testClassifier(String trainingPath, EmailClassifier emailClassifier) throws IOException {
+        CrossValidation cv = new CrossValidation(trainingPath, emailClassifier);
+        cv.fold(10);
+        cv.getCombinedConfusion().print();
     }
 
     public static void main(String[] args) throws IOException {
@@ -23,11 +32,8 @@ public class Train {
         //weightingMethod = new TfidfWeighting();
         weightingMethod = new FrequencyWeighting();
 
-        // Load the list of files and select 0.9 as training data and 0.1 as test data.
-        EmailClassifier emailClassifier = new EmailClassifier(new NaiveBayes(), weightingMethod, true);
-
-        CrossValidation cv = new CrossValidation(trainingPath, emailClassifier);
-        cv.fold(10);
-        cv.getCombinedConfusion().print();
+        Classifier classifier = new NaiveBayes();
+        EmailClassifier emailClassifier = new EmailClassifier(classifier, weightingMethod, true);
+        testClassifier(trainingPath, emailClassifier);
     }
 }
