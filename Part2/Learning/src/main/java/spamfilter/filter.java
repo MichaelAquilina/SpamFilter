f515c2d04a9bd799ac71924a350f7bae27d5a851
Michael Aquilina <michaelaquilina@gmail.com>
2013-12-08 17:35:20 +0000

    Implement filter.java

--- Part2/Learning/src/main/java/spamfilter/filter.java
+++ Part2/Learning/src/main/java/spamfilter/filter.java
@@ -1,7 +1,43 @@
 package spamfilter;
 
-/**
- * Created by michaela on 07/12/13.
- */
+import classification.EmailClass;
+import classification.EmailClassifier;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class filter {
+
+    private static final String DEFAULT_MODEL_PATH = "/home/michaela/Development/SpamFilter/model.json";
+
+    public static void usage() {
+        System.out.println("filter <targetFile>");
+        System.exit(1);
+    }
+
+    public static void main(String args[]) throws FileNotFoundException {
+        if(args.length != 1)
+            usage();
+
+        File modelFile = new File(DEFAULT_MODEL_PATH);
+        File targetFile = new File(args[0]);
+
+        if(modelFile.exists()) {
+            EmailClassifier emailClassifier = EmailClassifier.load(DEFAULT_MODEL_PATH);
+
+            try {
+                EmailClass emailClass = emailClassifier.classify(targetFile);
+
+                System.out.println(emailClass.toString());
+            } catch(IOException e) {
+                System.err.format("Unable to classify file \"%s\"\n", targetFile.getName());
+                e.printStackTrace();
+            }
+        }
+        else {
+            System.err.format("No Default model (%s) found. Have you performed training yet?\n", DEFAULT_MODEL_PATH);
+            System.exit(1);
+        }
+    }
 }
