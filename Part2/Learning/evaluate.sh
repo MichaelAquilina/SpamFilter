dde489eb61be2964886d0f2e2a85e9ab6025b334
Uwe L. Korn <uwelk@xhochy.com>
2013-12-10 23:21:16 +0100

    Fix evaluate

--- Part2/Learning/evaluate.sh
+++ Part2/Learning/evaluate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-for i in `seq 0.0 0.01 0.05`; do
-    for j in `seq 0.05 0.01 0.1`; do
+for i in `seq 0.0 0.001 0.02`; do
+    for j in `seq 0.5 0.1 1`; do
         echo $i $j
-        java -Xmx5g -jar target/spamfilter-learning_2.10-0.1.0-jar-with-dependencies.jar ../../traindata/ runlogs/classifier-$i-$j | tee runlogs/training-$i-$j
+        java -Xmx5g -jar target/spamfilter-learning_2.10-0.1.0-jar-with-dependencies.jar ../../traindata/ runlogs/classifier-$i-$j $i $j | tee runlogs/training-$i-$j
     done
 done
