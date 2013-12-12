b331d613abb6122df8ea187f76aa0bd6a3147377
Uwe L. Korn <uwelk@xhochy.com>
2013-12-12 20:19:57 +0100

    Add more runlogs

--- Part2/Learning/evaluate.sh
+++ Part2/Learning/evaluate.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-for i in `seq 0.0 0.001 0.02`; do
-    for j in `seq 0.5 0.1 1`; do
-        echo $i $j
-        java -Xmx5g -jar target/spamfilter-learning_2.10-0.1.0-jar-with-dependencies.jar ../../traindata/ runlogs/classifier-$i-$j $i $j | tee runlogs/training-$i-$j
+for k in `seq 1 1 1`; do
+    for i in `seq 0.001 0.001 0.1`; do
+        j=1
+        #for j in `seq 0.5 0.1 1`; do
+            echo $i $j
+            java -Xmx5g -jar target/spamfilter-learning_2.10-0.1.0-jar-with-dependencies.jar ../../traindata/ runlogs/classifier-$i-$j-$k $i $j | tee runlogs/training-$i-$j-$k
+        #done
     done
 done
