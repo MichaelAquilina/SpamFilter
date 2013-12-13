d84b455005ae22b4bfbc9cb34a2fec30319ada39
Uwe L. Korn <uwelk@xhochy.com>
2013-12-13 15:40:58 +0100

    Add more runlogs
    This will hopefully be the last runlogs for Naive bayes.

--- Part2/Learning/evaluate2.sh
+++ Part2/Learning/evaluate2.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 for k in `seq 1 1 1`; do
-    for i in `seq 0.002 0.0001 0.003`; do
-        j=0.19
-        #for j in `seq 0.5 0.1 1`; do
+    for i in `seq 0.0 0.1 0.0`; do
+        for j in `seq 0.1 0.01 0.2`; do
             echo $i $j
             java -Xmx5g -jar target/spamfilter-learning_2.10-0.1.0-jar-with-dependencies.jar ../../traindata/ runlogs/classifier-$i-$j-$k $i $j | tee runlogs/training-$i-$j-$k
-        #done
+        done
     done
 done
