7359400eb9ac2f3017c18fb7550a466c62b2c873
Uwe L. Korn <uwelk@xhochy.com>
2013-12-14 00:34:50 +0100

    Add J48 runlogs

--- Part2/Learning/src/main/java/weka/J48.java
+++ Part2/Learning/src/main/java/weka/J48.java
@@ -1,7 +1,13 @@
 package weka;
 
 public class J48 extends WekaClassifier {
-    public J48() {
+    public J48(int minInstances) {
         wekaClassifier = new weka.classifiers.trees.J48();
+        try {
+            wekaClassifier.setOptions(new String[]{"-D", Integer.toString(minInstances)});
+        } catch (Exception e) {
+            System.err.println("Could not setup J48 parameters");
+            e.printStackTrace();
+        }
     }
 }
