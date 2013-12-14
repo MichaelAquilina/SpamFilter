535361650c51ff13d6e93cfdd5af6f5afa37c050
Uwe L. Korn <uwelk@xhochy.com>
2013-12-14 02:34:26 +0100

    More J48 runlogs

--- Part2/Learning/evaluate3.sh
+++ Part2/Learning/evaluate3.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 for k in `seq 2 1 10`; do
-    for i in `seq 0.01 0.02 0.16`; do
+    i=0.017
+    #for i in `seq 0.01 0.02 0.16`; do
         j=0.19
         #for j in `seq 0.1 0.01 0.2`; do
             echo $i $j $k
             time java -Xmx5g -jar target/spamfilter-learning_2.10-0.1.0-jar-with-dependencies.jar ../../traindata/ runlogs/classifier-$i-$j-$k $i $j $k | tee runlogs/j48-training-$i-$j-$k
         #done
-    done
+    #done
 done
